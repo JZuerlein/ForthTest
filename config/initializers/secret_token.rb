e0a9d1f1aa8c92c849fa41acf066b4cc829f49ba
unknown <Jeff@JeffTower.(none)>
2013-01-15 10:01:20 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForthTest::Application.config.secret_token = '92aadaba05216196afa8a8fb3aba4e1c6ed832c0eaf97558c45320f852bfa310ebb8d675d5642fd12d0e4de9f9bc8fb7d1ab8509185a9d3ce0656c70f5636378'
